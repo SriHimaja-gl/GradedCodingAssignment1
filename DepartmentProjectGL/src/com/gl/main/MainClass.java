36eac847d1b187a501571b59bcdca236d8488711
SriHimaja-gl <107796111+SriHimaja-gl@users.noreply.github.com>
2022-06-30 19:42:01 +0530

    Add files via upload

---
+++ DepartmentProjectGL/src/com/gl/main/MainClass.java
@@ -0,0 +1,37 @@
+package com.gl.main;
+
+import com.gl.departments.*;
+
+public class MainClass 
+{
+
+	public static void main(String[] args) 
+	{
+		AdminDepartment ad = new AdminDepartment();
+		System.out.println("Welcome to "+ad.departmentName());
+		System.out.println(ad.getTodaysWork());
+		System.out.println(ad.getWorkDeadline());
+		System.out.println(ad.isTodayAHoliday());
+		
+		System.out.println();
+		System.out.println();
+		
+		HRDepartment hd = new HRDepartment();
+		System.out.println("Welcome to "+hd.departmentName());
+		System.out.println(hd.doActivity());
+		System.out.println(hd.getTodaysWork());
+		System.out.println(hd.getWorkDeadline());
+		System.out.println(hd.isTodayAHoliday());
+		
+		System.out.println();
+		System.out.println();
+		
+		TechDepartment td = new TechDepartment();
+		System.out.println("Welcome to "+td.departmentName());
+		System.out.println(td.getTodaysWork());
+		System.out.println(td.getWorkDeadline());
+		System.out.println(td.getTechStackInformation());
+		System.out.println(td.isTodayAHoliday());
+	}
+
+}
